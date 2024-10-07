69a39e43eb769ff4f7d3741f3c66acafc1c5c5c0
bryndalski <kubabryndal@gmail.com>
2024-10-07 15:07:09 +0200

    (ADDED)  - authorized page

--- src/src/pages/AuhtorizedPage.tsx
+++ src/src/pages/AuhtorizedPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, Paper, Typography } from '@mui/material';
 
 const AuthorizedPage = () => {
   const navigate = useNavigate();
@@ -20,22 +20,40 @@ const AuthorizedPage = () => {
         height: '100vh',
         textAlign: 'center',
         padding: 4,
+        background: 'linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%)',
       }}
     >
-      <Typography variant="h3" component="h1" gutterBottom>
-        Authorized Access
-      </Typography>
-      <Typography variant="body1" gutterBottom>
-        Welcome to the authorized page.
-      </Typography>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={handleNavigation}
-        sx={{ marginTop: 2 }}
+      <Paper
+        elevation={3}
+        sx={{
+          padding: 4,
+          borderRadius: 2,
+          backgroundColor: 'rgba(255, 255, 255, 0.8)',
+          backdropFilter: 'blur(10px)',
+        }}
       >
-        Go to Another Page
-      </Button>
+        <Typography variant="h3" component="h1" gutterBottom>
+          Authorized Access
+        </Typography>
+        <img
+          src={
+            'https://media.tenor.com/s7Tf_aL-Di0AAAAM/chipi-chipi-chapa-chapa.gif'
+          }
+          alt="Dancing Cat"
+          style={{ borderRadius: '10px', marginBottom: '20px' }}
+        />
+        <Typography variant="body1" gutterBottom>
+          Welcome to the authorized page.
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleNavigation}
+          sx={{ marginTop: 2 }}
+        >
+          Go to Another Page
+        </Button>
+      </Paper>
     </Box>
   );
 };
